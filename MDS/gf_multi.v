56350e33ad8af47609391e5d200752e7803a9666
Hasan Ali <143880255+hasan-03@users.noreply.github.com>
2025-04-03 23:31:21 +0530

    Update gf_multi.v

--- MDS/gf_multi.v
+++ MDS/gf_multi.v
@@ -1,24 +1,27 @@
 `timescale 1ns / 1ps
 
-module gf_multi (
-    input [7:0] U,   
-    input [7:0] V,   
-    output reg [7:0] W 
-);
+module multi(A,B,P,Out);
+    input [7:0] A ,B; 
+    input [8:0] P;
+    output reg [7:0] Out;
+    reg [8:0]temp;
     integer i;
-    reg [8:0] temp; 
-    parameter [8:0] P = 9'b101101001;
-    always @(*) begin
-        temp = 0;  
-        for (i = 1; i <= 8; i = i + 1) begin
-            temp = temp << 1; 
-            if (temp[8]) 
-                temp = temp ^ P;
-
-            if (U[8 - i])
-                temp = temp ^ V;
-        end  
-        W = temp[7:0]; 
-    end
-
+    
+    always @ (A,B,P)
+        begin
+        temp=0;
+        for (i=1; i<=8 ; i=i+1)
+            begin
+            
+            temp={temp,1'b0};
+            
+            if (temp[8]) temp=temp^P; 
+            else temp=temp;
+            
+            if (A[8-i]) temp=temp^B;
+            else temp=temp ;
+            
+            end    
+        Out=temp[7:0];
+        end
 endmodule
